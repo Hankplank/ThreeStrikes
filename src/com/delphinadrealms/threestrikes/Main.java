00e12a2dc562414fd48422f15c08c3fd76403147
BuildTools <unconfigured@null.spigotmc.org>
2017-10-22 14:14:28 -0500

    Initial commit

---
+++ src/com/delphinadrealms/threestrikes/Main.java
@@ -0,0 +1,20 @@
+package com.delphinadrealms.threestrikes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by henry27 on 10/22/2017.
+ */
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+
+}
